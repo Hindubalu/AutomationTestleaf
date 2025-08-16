4228f0e65a582444c6eb1c81cace7a49de86a300
Hindumathi <131352833+Hindubalu@users.noreply.github.com>
2025-08-16 10:51:11 +0530

    Add files via upload

---
+++ SeleniumAutomation/week1/day1/Datatypes.java
@@ -0,0 +1,39 @@
+package week1.day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n\t\tDATA TYPES\n");
+		byte byte1=45;
+		short short1=-3434;
+		int int1=3839434;
+		long long1=89324782394l;
+		float float1=83.38f;
+		double double1=39.9343;
+		boolean boolean1=true;
+		char char1='B';
+		System.out.println("Byte Value: "+byte1);
+		System.out.println("Short Value: "+short1);
+		System.out.println("Integer Value: "+int1);
+		System.out.println("Long Value: "+long1);
+		System.out.println("Float Value: "+float1);
+		System.out.println("Double Value: "+double1);
+		System.out.println("Boolean Value: "+boolean1);
+		System.out.println("Character Value: "+char1);
+		
+		System.out.println("\ndatatypes workout.........");
+		
+		float float12=483.35F;
+		long long12=89324782394L;
+		char char12='\uface';
+		
+		System.out.println("Maximun range of integer: "+Integer.MAX_VALUE);
+		System.out.println("Size of short: "+ Short.SIZE);
+		System.out.println("Minimum range of byte: "+Byte.MIN_VALUE);
+		System.out.println(float12);
+		System.out.println(long12);
+		System.out.println(char12);
+	}
+
+}
