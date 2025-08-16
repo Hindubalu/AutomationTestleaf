1a8536f9790915b1645ad126ff8a80585bca01e5
Hindumathi <131352833+Hindubalu@users.noreply.github.com>
2025-08-16 10:52:06 +0530

    Add files via upload

---
+++ SeleniumAutomation/week1/day3/ConditionalStmts.java
@@ -0,0 +1,28 @@
+package week1.day3;
+
+public class ConditionalStmts {
+
+	public static void main(String[] args) {
+		
+		
+		  int n1=25;  
+		  int n2=39;  
+		  if(n1!=n2){  
+			  System.out.println("25!=39");
+			  if(n1<n2){  
+				  System.out.println("25<39");  
+				  if(n1<=n2) { 
+					  System.out.println("25<=39");
+					  } 
+				  }  
+			  }  
+		  else { 
+			  System.out.println("Does not fall under any condition"); 
+			  }
+		 	 
+
+		
+		
+	}
+
+}
