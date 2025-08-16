09ab33b250d4f79635a4a7a5142393fb9edb2e4a
Hindumathi <131352833+Hindubalu@users.noreply.github.com>
2025-08-16 10:57:40 +0530

    Add files via upload

---
+++ SeleniumAutomation/week4/day1/MethodOverriding.java
@@ -0,0 +1,24 @@
+package week4.day1;
+
+public class MethodOverriding extends MethodOverloading{
+	
+	//Java example--> .equals()
+	//Selenium example--> river.quit();
+	
+	public void add() {
+		
+
+	}
+	public void add(int a ,int b) {
+
+		System.out.println(a+b);
+		
+	}
+		
+	public void add(float a,float b) {
+
+		System.out.println(a-b);
+		
+	}
+	
+}
