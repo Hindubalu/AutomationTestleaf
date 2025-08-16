1e73600aa5394a1ab57df1664c4635ed592ff978
Hindumathi <131352833+Hindubalu@users.noreply.github.com>
2025-08-16 10:53:58 +0530

    Add files via upload

---
+++ SeleniumAutomation/week2/day3/LaunchBrowser.java
@@ -0,0 +1,11 @@
+package week2.day3;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.close();
+	}
+
+}
